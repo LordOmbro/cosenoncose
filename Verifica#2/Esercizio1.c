3d16c1f6e58d6b3bcbb9d00164e6aed52390c6a0
thatsed <>
2017-12-21 14:50:43 +0100

    Update: menu

--- Verifica#2/Esercizio1.c
+++ Verifica#2/Esercizio1.c
@@ -34,7 +34,27 @@ typedef tipoelemento *tipolista;
 
 void _inizializza(tipolista *lista);
 bool _isEnd(tipolista lista);
-void _inserisci_coda(tipolista *lista, tipopersona dati);
-void _inserisci_testa(tipolista *lista, tipopersona dati);
-void _inserisci_testa(tipolista *lista, tipopersona dati);
+void _inserisci_coda(tipolista *lista, tipodato dati);
+void _inserisci_testa(tipolista *lista, tipodato dati);
+void _inserisci_testa(tipolista *lista, tipodato dati);
 void _elimina_posizione(tipolista *lista);
+void _stampa(tipolista lista);
+void _media(tipolista lista);
+
+int main(){
+	tipolista lista;
+	int sel;
+	_inizializza(&lista);
+	while(true){
+		printf("\nMENU\n");
+		printf("1. Inserisci prenotazione\n");
+		printf("2. Elimina prenotazione\n");
+		printf("3. Stampa prenotazione in attesa\n");
+		printf("4. Calcola importo medio per prenotazione\n");
+		printf("\n0. Inserisci prenotazione\n");
+		printf("\nSelezione: ");
+
+
+
+	}
+}
