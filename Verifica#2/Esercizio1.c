a83b1e694fba6a7a1b67617e79c58817f3aebc5b
thatsed <>
2017-12-21 14:42:02 +0100

    Inizio: struct

--- Verifica#2/Esercizio1.c
+++ Verifica#2/Esercizio1.c
@@ -6,8 +6,23 @@
 typedef char str[MAX_LEN];
 typedef enum {false,true} bool;
 
-typedef struct DATO {
+typedef struct DATA {
+	int giorno;
+	int mese;
+	int anno;
+} tipodata;
 
+typedef struct ORA {
+	int h;
+	int m;
+} tipoora;
+
+typedef struct DATO {
+	int id;
+	tipodata data;
+	tipoora ora;
+	int articoli;
+	float importo;
 } tipodato;
 
 typedef struct EL {
@@ -16,3 +31,10 @@ typedef struct EL {
 } tipoelemento;
 
 typedef tipoelemento *tipolista;
+
+void _inizializza(tipolista *lista);
+bool _isEnd(tipolista lista);
+void _inserisci_coda(tipolista *lista, tipopersona dati);
+void _inserisci_testa(tipolista *lista, tipopersona dati);
+void _inserisci_testa(tipolista *lista, tipopersona dati);
+void _elimina_posizione(tipolista *lista);
