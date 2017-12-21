e909371d17f580e7ea46c4e3dd93045e4ecb12f9
thatsed <>
2017-12-21 15:00:15 +0100

    Update: qualcosina

--- Verifica#2/Esercizio1.c
+++ Verifica#2/Esercizio1.c
@@ -34,16 +34,16 @@ typedef tipoelemento *tipolista;
 
 void _inizializza(tipolista *lista);
 bool _isEnd(tipolista lista);
-void _inserisci_coda(tipolista *lista, tipodato dati);
-void _inserisci_testa(tipolista *lista, tipodato dati);
-void _inserisci_testa(tipolista *lista, tipodato dati);
+void _inserisci_dati(tipolista *lista)
+void _inserisci(tipolista *lista, tipodato dati);
+void _elimina_testa(tipolista *lista);
 void _elimina_posizione(tipolista *lista);
 void _stampa(tipolista lista);
 void _media(tipolista lista);
 
 int main(){
 	tipolista lista;
-	int sel;
+	int sel,id;
 	_inizializza(&lista);
 	while(true){
 		printf("\nMENU\n");
@@ -53,8 +53,52 @@ int main(){
 		printf("4. Calcola importo medio per prenotazione\n");
 		printf("\n0. Inserisci prenotazione\n");
 		printf("\nSelezione: ");
-
-
-
+		scanf("%d",&sel);
+		switch(sel){
+			case 0:
+				printf("\nNegozio chiuso!\n");
+				break;
+			case 1:
+				_inserisci_dati(&lista);
+				break;
+			case 2:
+				// SOTTOMENU: elimina testa o in mezzo -- NON NECESSARIO
+				system("clear");
+				printf("\nSOTTOMENU: ELIMINAZIONE\n");
+				printf("1. Elimina prenotazione più vecchia\n");
+				printf("2. Elimina prenotazione per ID\n");
+				printf("\n0. Torna al menu principale\n");
+				printf("\nSelezione: \n");
+				scanf("%d",&sel);
+				switch(sel){
+					case 1:
+						break;
+					case 2:
+						break;
+					default:
+						break;
+				}
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				printf("Selezione non valida\n");
+				break;
+		}
+		printf("\nPremi ENTER per tornare al menu\n");
+		system("read");
+		system("clear");
 	}
+}
+
+void _inizializza(tipolista *lista){
+	*lista=NULL;
+}
+
+bool _isEnd(tipolista lista){
+	if(lista==NULL)
+		return true;
+	return false;
 }
