be86e20c8114b2d4ed2e286fb3c53b037d997791
thatsed <>
2017-12-23 17:57:19 +0100

    Assembly

---
+++ cagata.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int i=-3;
+	while(1){
+		if(i)
+			return 0;
+		i++;
+	}
+}
---
+++ cagata.s
@@ -0,0 +1,30 @@
+	.file	"cagata.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$-3, -4(%rbp)
+.L4:
+	cmpl	$0, -4(%rbp)
+	je	.L2
+	movl	$0, %eax
+	jmp	.L5
+.L2:
+	addl	$1, -4(%rbp)
+	jmp	.L4
+.L5:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 6.3.0-18) 6.3.0 20170516"
+	.section	.note.GNU-stack,"",@progbits
